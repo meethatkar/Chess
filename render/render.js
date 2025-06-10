7b6e4b7fed1075649c77a4bfbd3e26a96ff7c8dc
meethatkar <meethatkar000@gmail.com>
2025-06-10 20:46:54 +0530

    rendered all peices in render.js and displayed on html page using else if nested conditions and block position

--- render/render.js
+++ render/render.js
@@ -1,5 +1,7 @@
-import { blackPawn } from "../Data/peices.js";
-import { whitePawn } from "../Data/peices.js";
+// import { blackPawn } from "../Data/peices.js";
+// import { whitePawn } from "../Data/peices.js";
+
+import * as peices from "../Data/peices.js"     //this is easy, as if not done this then nee dot write import statement 12 times
 
 const rootDive = document.getElementById("root");
 
@@ -24,11 +26,47 @@ function initGameRender(data){
         element.squareRowArr.forEach((square)=>{     //first element of first array and so on
             if(element.rowID==7){
                 // console.log("black"); 
-                square.peice = blackPawn(square.id);        //send current pos as paramenter, and returns img path and current pos
+                square.peice = peices.blackPawn(square.id);        //send current pos as paramenter, and returns img path and current pos
             }
             else if(element.rowID==2){
                 // console.log("white");
-                square.peice = whitePawn(square.id);        
+                square.peice = peices.whitePawn(square.id);        
+            }
+            else if(element.rowID==8){
+                // MAIN BLACK PEICES
+                if(square.id=="e8"){
+                    square.peice = peices.kingBlack(square.id);
+                }
+                if(square.id=="d8"){
+                    square.peice = peices.queenBlack(square.id);
+                }
+                if(square.id=="c8" || square.id=="f8"){
+                    square.peice = peices.camelBlack(square.id);
+                }
+                if(square.id=="b8" || square.id=="g8"){
+                    square.peice = peices.horseBlack(square.id);
+                }
+                if(square.id=="a8" || square.id=="h8"){
+                    square.peice = peices.elephantBlack(square.id);
+                }
+            }
+            else if(element.rowID==1){
+                // MAIN WHITE PEICES
+                if(square.id=="e1"){
+                    square.peice = peices.kingWhite(square.id);
+                }
+                if(square.id=="d1"){
+                    square.peice = peices.queenWhite(square.id);
+                }
+                if(square.id=="c1" || square.id=="f1"){
+                    square.peice = peices.camelWhite(square.id);
+                }
+                if(square.id=="b1" || square.id=="g1"){
+                    square.peice = peices.horseWhite(square.id);
+                }
+                if(square.id=="a1" || square.id=="h1"){
+                    square.peice = peices.elephantWhite(square.id);
+                }
             }
             var squareDiv = document.createElement("div");
             squareDiv.id = square.id;
